6f5a3d324f1c1722bb4f440cc9765b99fa03bce4
Your Name <you@example.com>
2025-01-17 20:12:21 -0400

    HBD

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const music = document.getElementById('backgroundMusic');
+const button = document.getElementById('playPauseButton');
+
+button.addEventListener('click', () => {
+    if (music.paused) {
+        music.play();
+        button.textContent = 'Pausar';
+    } else {
+        music.pause();
+        button.textContent = 'Reproducir';
+    }
+});
+
+(function() {
+  function $(id) {
+    return document.getElementById(id);
+  }
+
+  var card = $('card'),
+      openB = $('open'),
+      closeB = $('close'),
+      timer = null;
+  console.log('wat', card);
+  openB.addEventListener('click', function () {
+    card.setAttribute('class', 'open-half');
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute('class', 'open-fully');
+      timer = null;
+    }, 1000);
+  });
+
+  closeB.addEventListener('click', function () {
+    card.setAttribute('class', 'close-half');
+    if (timer) clearTimerout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute('class', '');
+      timer = null;
+    }, 1000);
+  });
+
+}());
